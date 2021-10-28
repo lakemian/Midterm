40073f11b172901f09f558e79af1cbfc8771e7b1
Laura Kemian <lakemian@umich.edu>
2021-10-28 16:09:06 -0400

    laura changes

--- AddIntegers.java
+++ AddIntegers.java
@@ -7,6 +7,7 @@ public class AddIntegers {
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
+        System.out.println("Laura Changes");
 
         System.out.println("The sum is: " + sum);
     }
